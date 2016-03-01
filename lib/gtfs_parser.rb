7589919b9872ea071a2c04a80195f89583939d8e
David Faulkenberry <david.faulkenberry@gmail.com>
2016-03-01 14:03:50 -0500

    GTFS parser returns last and next departures

--- lib/gtfs_parser.rb
+++ lib/gtfs_parser.rb
@@ -15,13 +15,23 @@ module GtfsParser
     cache_departures!
   end
 
+  # Returns two departure times:
+  # 1. The departure scheduled to have left most recently:
+  # 2. The departure scheduled to leave soonest (within 3 hours).
   def soonest_departures_within(hours)
     departure_times = cached_departures
     departure_times.each do |route_number, times|
-      departure_times[route_number] = times.find do |time|
-        time_within? hours, time
+      last_time = times.first
+      next_time = times.first
+      times.each_with_index do |time, i|
+        next unless time_within? hours, time
+        next_time = time
+        last_time = times[i - 1] if i > 0
+        break
       end
+      departure_times[route_number] = [last_time, next_time]
     end
+    departure_times
   end
 
   private
