288ea08a6bd72d722b185112e24ee0bc2001f4fe
Molly O'Neil <mboneil@umass.edu>
2016-12-20 15:45:51 -0500

    Test compare in departure_comparator

--- spec/departure_comparator_spec.rb
+++ spec/departure_comparator_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 include DepartureComparator
+include GtfsParser
 
 describe DepartureComparator do
   before :each do
@@ -33,7 +34,7 @@ describe DepartureComparator do
   end
   describe 'report_missing_route' do
     context 'with parameters of route_number, headsign, gtfs_time' do
-      it 'adds missing_routes to statuses and messages' do
+      it 'adds missing_route to statuses and messages' do
         route_and_headsign = "Route #{@route_number} with headsign #{@headsign} is missing:"
         expected_departure = "Expected to be departing from Studio Arts Building"
         expected_time = "Expected scheduled departure time #{email_format @gtfs_time}"
@@ -60,6 +61,35 @@ describe DepartureComparator do
         expect(@messages.last).to include expected_time
         expect(@messages.last).to include avail_time
         expect(@statuses.fetch :incorrect_times).to include @route_number
+      end
+    end
+  end
+  describe 'compare' do
+    context 'with scheduled routes' do
+      it 'checks if every route is present and next departure has correct scheduled time' do
+        Bojangles.stub(:get_avail_departure_times!) do
+          {["30", "0", "North Amherst"]=>
+            ["07:28:08",
+             "07:35:03",
+             "08:01:40",
+             "08:20:00",
+             "08:31:38",
+             "08:46:50",
+             "09:03:19",
+             "09:20:20",
+             "09:35:40",
+             "09:45:25",
+             "10:00:22",
+             "10:15:10"]}
+        end
+        departures = find_departures
+        File.open 'cached_departures.json', 'w' do |file|
+          file.puts departures.to_json
+        end
+        result = compare
+
+        expect(result.first).to be @messages
+        expect(result.last).to be @statuses
       end
     end
   end
