7342f5f34f2a59dcd866d10b083f60eb4cfd3170
Molly O'Neil <mboneil@umass.edu>
2017-04-05 10:38:20 -0400

    Test multiple stops in find_depts

---
+++ spec/gtfs_parser_spec.rb
@@ -0,0 +1,16 @@
+#frozen_string_literal: true
+require 'spec_helper'
+
+describe GtfsParser do
+	describe 'find_departures' do
+		context 'with multiple stops' do
+			it 'finds departures at that route' do 
+				stops = ['North Amherst', 'Graduate Research Center']
+				result = find_departures(stops)
+				# hash keyed by route ID, direction, and headsign to an array of times
+				expect(result.key? ['31', '31', '0', 'Sunderland'])
+				expect(result[['31', '31', '0', 'Sunderland']]).is_a? Array
+			end
+		end
+	end
+end
