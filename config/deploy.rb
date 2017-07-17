291aec317b73f93cb865d3d0efd8ab5359514a92
David Faulkenberry <david.faulkenberry@gmail.com>
2017-07-17 15:35:06 -0400

    Don't need to set Git as SCM

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,6 @@ set :application, "bojangles"
 set :repo_url, "git@github.com:umts/bojangles.git"
 set :branch, :master
 set :deploy_to, "/srv/#{fetch :application}"
-set :scm, :git
 set :keep_releases, 5
 
 set :whenever_command, %i(sudo bundle exec whenever)
