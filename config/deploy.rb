4c5ff4c8e157768321122126ba68fc9b8c9d0c8b
David Faulkenberry <david.faulkenberry@gmail.com>
2017-07-18 11:03:12 -0400

    Linked files and directories

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,3 +7,14 @@ set :deploy_to, "/srv/#{fetch :application}"
 set :keep_releases, 5
 
 set :whenever_command, %i(sudo bundle exec whenever)
+
+set :linked_files, fetch(:linked_files, []).push(
+  'cached_departures.json',
+  'config.json',
+  'route_mappings.json'
+)
+
+set :linked_dirs, fetch(:linked_dirs, []).push(
+  'gtfs',
+  'log'
+)
